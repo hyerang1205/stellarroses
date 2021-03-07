cdd6e59c25be225610b9bc970fd703c9c5ba8110
Lena Kwan <lenakwan@gmail.com>
2021-03-06 18:47:48 -0800

    api calls to domain/v1/sealevel/:id now returns json

--- api/v1/controller/seaLevelController.js
+++ api/v1/controller/seaLevelController.js
@@ -12,7 +12,9 @@ getUserLevels = async (req, res) => {
             console.log(data.rows);
             let sea_levels = [];
             for(i = 1; i < 101; i ++){
-                sea_levels.push(97 + i* data.rows[0].multiplier);
+                let current_sea = 97 + i* data.rows[0].multiplier;
+                let current_levels = {year: parseInt(data.rows[0].current_year) + i, level: current_sea.toFixed(2)}
+                sea_levels.push(current_levels)
             }
             res.status(200).json(sea_levels);
         }).catch(err => res.status(500).json({
